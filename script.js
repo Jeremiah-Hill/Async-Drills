b336d9f1fea7ebbf62602035f7693e8ab5629c85
Jeremiah-Hill <77300280+Jeremiah-Hill@users.noreply.github.com>
2021-02-22 09:59:36 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('Start');
+    doStuff();
+    console.log('Finished');
+});
+
+function doStuff() {
+    function repeat(message) {
+        console.log(`${message}`);
+    }
+    repeat("Howdy Earth");
+    setTimeout(() => {
+        repeat("Long Time No See, Gardener");
+    }, 2000);
+
+    function getWords(w1, w2, w3, w4) {
+        console.log(w1);
+        setTimeout(() => {
+            console.log(w2);
+            setTimeout(() => {
+                console.log(w3);
+                setTimeout(() => {
+                    console.log(w4);
+                }, 1000);
+            }, 2000);
+        }, 3000);
+    }
+    getWords("instant", "three", "two", "one");
+}
+
+function done() {
+    console.log("Job's done!");
+}
+function countdown(num, callback) {
+    if (num > 0) {
+        console.log(num);
+        setTimeout(() => {
+            countdown(num - 1, callback);
+        }, 1000);
+    } else if (num === 0) {
+        callback();
+    } else {
+        console.log("error");
+    }
+}
+countdown(5, done);
+
+let lunchTime = true;
+function orderMeSomeFood() {
+    return new Promise((resolve, reject) => {
+        if (lunchTime) {
+            let lunchObj = {
+                lunch: 'Grizzly Polar Bear Buger',
+                drink: 'Sprite'
+            }
+            resolve(lunchObj);
+        } else {
+            let lunchErr = new Error('Not lunch!');
+            reject(lunchErr);
+        }
+    });
+}
+function order() {
+    orderMeSomeFood()
+    .then((value) => {
+        console.log(value);
+    }).catch((err) => {
+        console.log(err);
+    });
+}
+order();
+lunchTime = false;
+order();
